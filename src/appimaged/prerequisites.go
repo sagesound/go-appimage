35dffc10098343e9962f9344d1d25aeb812d9740
sagesound <97154903+sagesound@users.noreply.github.com>
2022-01-05 00:26:54 -0800

    Update prerequisites.go

--- src/appimaged/prerequisites.go
+++ src/appimaged/prerequisites.go
@@ -18,7 +18,7 @@ import (
 	"strings"
 
 	systemddbus "github.com/coreos/go-systemd/v22/dbus"
-	"github.com/probonopd/go-appimage/internal/helpers"
+	"github.com/sagesound/go-appimage/internal/helpers"
 	"github.com/shirou/gopsutil/process"
 )
 
